81844f62d539446092591adba57735ca59c424ca
Michael S. Noble <mnoble@broadinstitute.org>
2016-05-23 11:05:29 -0400

    generate.py

---
+++ generate.py
@@ -0,0 +1,23 @@
+
+# Experimental code, to explore how to best automate wrapper generation
+
+import GDCcore as core
+from pprint import pprint
+
+core.set_codec(core.CODEC_DJSON)
+Endpoints=["projects", "cases", "files", "annotations"]
+Endpoints=["files"]
+
+fields = {}
+
+for ep in Endpoints:
+    mapping = core.get(ep + "/_mapping")
+    print("\n\n%s/_mapping contains: %s" % (ep, str(mapping.keys())))
+    print(ep + " endpoint supports the query fields: ")
+    for field in mapping["fields"]:
+        print("\t" + field)
+        terms = field.split(".")
+        terms.reverse()
+        fields.setdefault(terms[0], []).extend(terms[1:])
+
+    pprint(fields)
