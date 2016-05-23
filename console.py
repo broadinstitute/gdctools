a580dba92e34748ee9c5123a8faba2f8f57e989f
Michael S. Noble <mnoble@broadinstitute.org>
2016-05-23 11:10:58 -0400

    remove vestigial mention of mdbtools

--- console.py
+++ console.py
@@ -1,11 +1,11 @@
 
-# Simple func for each mdbtool to be used from the CLI: infer the tool name at
+# Simple func for each gdctool to be used from the CLI: infer the tool name at
 # runtime, instantiate an object of that class, then call its execute() method
 
 def console():
     import os
     import sys
-    import mdbtools
+    import gdctools
     tool = os.path.basename(sys.argv[0])
-    klass = getattr(mdbtools, tool)
+    klass = getattr(gdctools, tool)
     klass().execute()
