f354d0c8db006a757e60a4fbebd05c358d79e302
Timothy DeFreitas <timdef@broadinstitute.org>
2016-05-24 09:35:46 -0400

    Adjust mirror folder structure, improve logging

--- gdc_mirror.py
+++ gdc_mirror.py
@@ -147,33 +147,47 @@ class gdc_mirror(GDCtool):
             for cat in data_categories:
                 #Replace spaces with underscores for better folder names
                 data_dir = os.path.join(proj_root, cat.replace(' ', '_'))
+                meta_dir = os.path.join(data_dir, "meta")
                 if not os.path.isdir(data_dir):
                     logging.info("Creating folder: " + data_dir)
                     os.makedirs(data_dir)
+                if not os.path.isdir(meta_dir):
+                    logging.info("Creating metadata folder: " + meta_dir)
+                    os.makedirs(meta_dir)
 
                 file_metadata = gdc.get_files(project, cat)
+                
+                # Save metadata in json format for dicing reference, in <data_category>/meta/
                 metadata_filename = '.'.join(["metadata", self.timestamp, "json"])
-                metadata_path = os.path.join(data_dir, metadata_filename)
-                # Save metadata in json format for dicing reference 
+                metadata_path = os.path.join(meta_dir, metadata_filename)
                 with open(metadata_path, 'w') as f:
                     f.write(json.dumps(file_metadata, indent=2))
 
-                # Download actual files
                 total_files = len(file_metadata)
-                logging.info("Downloading " + str(total_files) + " " + cat + " files")
+                logging.info("Mirroring " + str(total_files) + " " + cat + " files")
+                file_num = 0
                 for file_d in file_metadata:
+                    file_num += 1
                     uuid = file_d['file_id']
                     name = file_d['file_name']
+                    dtype = file_d['data_type']
 
-                    savepath = os.path.join(data_dir, name)
+                    type_folder = os.path.join(data_dir, dtype.replace(' ', '_'))
+                    
+                    #if the data type folder doesn't yet exist, create it
+                    if not os.path.isdir(type_folder):
+                        os.makedirs(type_folder)
+
+                    # Download actual files to <data_category>/<data_type>/<file>[.md5]
+                    savepath = os.path.join(type_folder, name)
                     md5path = savepath + ".md5"
 
                     #If we don't already have a checksum for this file, download both the file and md5
+                    #TODO: verify MD5 matches file metadata, to confirm file identity
                     if os.path.isfile(md5path):
                         logging.info("File " + name + " already exists, skipping download")
                     else:
-                        logging.info("Downloading file " + name ) 
-
+                        logging.info("Downloading file {0}, {1} of {2}".format(name, file_num, total_files))
                         retry_count = 3
                         while retry_count > 0:
                             try:
