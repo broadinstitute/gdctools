cfb59f35ee9aac8bde44965233cf3db64fe83dc3
Timothy DeFreitas <timdef@broadinstitute.org>
2016-05-24 15:55:01 -0400

    Add metadata-only option to gdc mirroring

--- gdc_mirror.py
+++ gdc_mirror.py
@@ -45,6 +45,8 @@ class gdc_mirror(GDCtool):
                          help='Mirror data from these cancer programs')
         cli.add_argument('-p', '--projects', nargs='+', metavar='project',
                          help='Mirror data from these projects')
+        cli.add_argument('-m', '--meta-only', action='store_true',
+                         help="Only retrieve metadata, skip file download")
 
         cli.add_argument('config', nargs='?', default=None,
                          help='GDC mirror configuration file')
@@ -171,48 +173,52 @@ class gdc_mirror(GDCtool):
                 with open(metadata_path, 'w') as f:
                     f.write(json.dumps(file_metadata, indent=2))
 
-                total_files = len(file_metadata)
-                logging.info("Mirroring " + str(total_files) + " " + cat + " files")
-                file_num = 0
-                for file_d in file_metadata:
-                    file_num += 1
-                    uuid = file_d['file_id']
-                    name = file_d['file_name']
-                    dtype = file_d['data_type']
+                if self.options.meta_only:
+                    logging.info("Metadata only option enabled, skipping file mirroring")
 
-                    type_folder = os.path.join(data_dir, dtype.replace(' ', '_'))
-                    
-                    #if the data type folder doesn't yet exist, create it
-                    if not os.path.isdir(type_folder):
-                        os.makedirs(type_folder)
+                else:
+                    total_files = len(file_metadata)
+                    logging.info("Mirroring " + str(total_files) + " " + cat + " files")
+                    file_num = 0
+                    for file_d in file_metadata:
+                        file_num += 1
+                        uuid = file_d['file_id']
+                        name = file_d['file_name']
+                        dtype = file_d['data_type']
 
-                    # Download actual files to <data_category>/<data_type>/<file>[.md5]
-                    savepath = os.path.join(type_folder, name)
-                    md5path = savepath + ".md5"
+                        type_folder = os.path.join(data_dir, dtype.replace(' ', '_'))
+                        
+                        #if the data type folder doesn't yet exist, create it
+                        if not os.path.isdir(type_folder):
+                            os.makedirs(type_folder)
 
-                    #If we don't already have a checksum for this file, download both the file and md5
-                    #TODO: verify MD5 matches file metadata, to confirm file identity
-                    if os.path.isfile(md5path):
-                        logging.info("File " + name + " already exists, skipping download")
-                    else:
-                        logging.info("Downloading file {0}, {1} of {2}".format(name, file_num, total_files))
-                        retry_count = 3
-                        while retry_count > 0:
-                            try:
-                                #Download file
-                                gdc.get_file(uuid, savepath)
-                                break
-                            except subprocess.CalledProcessError as e:
-                                logging.warning("Curl call failed: " + str(e))
-                                retry_count = retry_count - 1
+                        # Download actual files to <data_category>/<data_type>/<file>[.md5]
+                        savepath = os.path.join(type_folder, name)
+                        md5path = savepath + ".md5"
 
-                        if retry_count == 0:
-                            logging.error("Error downloading file " + savepath + ", too many retries (3)")
+                        #If we don't already have a checksum for this file, download both the file and md5
+                        #TODO: verify MD5 matches file metadata, to confirm file identity
+                        if os.path.isfile(md5path):
+                            logging.info("File " + name + " already exists, skipping download")
                         else:
-                            #Save md5 checksum on success
-                            md5sum = file_d['md5sum']
-                            with open(md5path, 'w') as mf:
-                                mf.write(md5sum + "  " + name )
+                            logging.info("Downloading file {0}, {1} of {2}".format(name, file_num, total_files))
+                            retry_count = 3
+                            while retry_count > 0:
+                                try:
+                                    #Download file
+                                    gdc.get_file(uuid, savepath)
+                                    break
+                                except subprocess.CalledProcessError as e:
+                                    logging.warning("Curl call failed: " + str(e))
+                                    retry_count = retry_count - 1
+
+                            if retry_count == 0:
+                                logging.error("Error downloading file " + savepath + ", too many retries (3)")
+                            else:
+                                #Save md5 checksum on success
+                                md5sum = file_d['md5sum']
+                                with open(md5path, 'w') as mf:
+                                    mf.write(md5sum + "  " + name )
         logging.info("Mirror completed successfully.")
 
 
