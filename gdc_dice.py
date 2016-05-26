35807126d8038b86dbd8fd21a450a0bfcd5db7d6
Timothy DeFreitas <timdef@broadinstitute.org>
2016-05-26 10:01:43 -0400

    update dicer to find latest mirror timestamp

--- gdc_dice.py
+++ gdc_dice.py
@@ -49,7 +49,7 @@ class gdc_dicer(GDCtool):
                          help='Mirror data from these cancer programs')
         cli.add_argument('-p', '--projects', nargs='+', metavar='project',
                          help='Mirror data from these projects')
-        cli.add_argument('datestamp', nargs='?',
+        cli.add_argument('datestamp',
                          help='Dice using metadata from a particular date.'\
                          'If omitted, the latest version will be used')
 
@@ -60,7 +60,6 @@ class gdc_dicer(GDCtool):
         diced_root = self.options.dice_directory
         trans_dict = translator.build_translation_dict(resource_filename(__name__,
                                                        "Harmonized_GDC_translation_table_FH.tsv"))
-        timestamp = timetuple2stamp()
 
         #Iterable of programs, either user specified or discovered from folder names in the diced root
         if self.options.programs is not None:
@@ -71,6 +70,8 @@ class gdc_dicer(GDCtool):
         for program in programs:
             diced_prog_root = os.path.join(diced_root, program)
             mirror_prog_root = os.path.join(mirror_root, program)
+
+
             if self.options.projects is not None:
                 projects = self.options.projects
             else:
@@ -79,18 +80,16 @@ class gdc_dicer(GDCtool):
             for project in projects:
                 raw_project_root = os.path.join(mirror_prog_root, project)
                 diced_project_root = os.path.join(diced_prog_root, project)
-
-                if self.options.datestamp is None:
-                    metadata = translator.get_metadata(raw_project_root)
-                else:
-                    metadata = translator.get_metadata(raw_project_root, self.options.datestamp)
+                logging.info("Dicing " + project + " to " + diced_project_root)
+                timestamp = get_mirror_timestamp(raw_project_root)
+                logging.info("Timestamp: " + timestamp)
+                metadata = translator.get_metadata(raw_project_root, self.options.datestamp)
 
                 for files in metadata:
                     if len(files) > 0:
                         for f in files:
                             translator.dice(f, trans_dict, raw_project_root, diced_project_root,
                          timestamp, dry_run=self.options.dry_run)
-
 
 
     def execute(self):
@@ -100,6 +99,33 @@ class gdc_dicer(GDCtool):
                             level=logging.INFO)
         self.dice()
 
+def get_mirror_timestamp(proj_root, datestamp):
+    """Get the timestamp of the last mirror run on the given date.
+
+    Does this by iterating over the meta folders and grabs the latest value.
+
+    TODO: Move the meta iterator to a util file
+    """
+    raw_project_root = proj_root.rstrip(os.path.sep)
+    project = os.path.basename(raw_project_root)
+    
+    latest_tstamp = None
+
+    for dirpath, dirnames, filenames in os.walk(raw_project_root, topdown=True):
+        # Only recurse down to meta subdirectories
+        if os.path.basename(os.path.dirname(dirpath)) == project:
+            for n, subdir in enumerate(dirnames):
+                if subdir != 'meta': del dirnames[n]
+        # Take the most recent version of the given datestamp
+        if os.path.basename(dirpath) == 'meta':
+            meta_files = sorted(filename for filename in filenames if \
+                                datestamp in filename)
+            if len(meta_files) > 0 :
+                # meta filenames = "metadata.<timestamp>.json"
+                timestamp = meta_files[-1].split(".")[1]
+                if latest_tstamp is None or timestamp > latest_tstamp:
+                    latest_tstamp = timestamp
+    return latest_tstamp
 
 def immediate_subdirs(path):
     return [d for d in os.listdir(path) 
