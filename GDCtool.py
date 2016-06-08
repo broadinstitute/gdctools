565466afeb59a1063325c75e896ad22a8d9a7bb2
Timothy DeFreitas <timdef@broadinstitute.org>
2016-06-08 16:17:15 -0400

    Move configuration options to GDCtool.py

--- GDCtool.py
+++ GDCtool.py
@@ -17,6 +17,8 @@ file for the SOFTWARE COPYRIGHT and WARRANTY NOTICE.
 import sys
 import os
 import traceback
+import ConfigParser
+import time
 from GDCcli import GDCcli
 from GDCcore import *
 
@@ -31,6 +33,55 @@ class GDCtool(object):
 
     def execute(self):
         self.options = self.cli.parse_args()
+        self.parse_config()
+
+    def __configure(self, option, cfg_parser, multi=False):
+        '''Gets the value of "option" from the cfg_parser or it's default
+        If multi is True, the option is split by ',' and returned as list
+        '''
+        today = time.strftime('%Y_%m_%d')
+        CONFIG_DEFAULTS = {
+            'mirror' : {
+                'root_dir': 'gdc_mirror__' + today,
+                'log_dir' : 'gdc_mirror_log__' + today,
+                'programs': ['TCGA'],
+                'projects': None
+            },
+            'dice' : {
+                'root_dir': 'gdc_mirror__' + today,
+                'log_dir' : 'gdc_mirror_log__' + today,
+                'programs': 'TCGA',
+                'projects': None
+            }
+        }
+        # All options are namespaced
+        sect, opt = option.split('_', 1)
+        value = CONFIG_DEFAULTS[sect].get(opt, None)
+        if cfg_parser is not None and cfg_parser.has_option(sect, opt):
+            value = cfg_parser.get(sect, opt)
+            if multi: value = value.strip().split(',')
+
+        # If value was set by cli flag, overwrite it
+        value = getattr(self.options, option, value)
+        # Store the option in self
+        setattr(self, option, value)
+        return value
+
+
+    def parse_config(self):
+        """Read options from config, and optionally override them with args"""
+        config_file = self.options.config
+        cfg = None
+        if config_file is not None:
+            cfg = ConfigParser.ConfigParser()
+            cfg.read(config_file)
+        CONFIGURABLES = [ 'mirror_root_dir', 'mirror_log_dir',
+                          'mirror_programs', 'mirror_projects',
+                          'dice_root_dir', 'dice_log_dir',
+                          'dice_programs', 'dice_projects']
+
+        for conf in CONFIGURABLES:
+            self.__configure(conf, cfg)
 
     def status(self):
         # Emit system info (as header comments suitable for TSV, etc) ...
