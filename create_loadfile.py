8f25842a6a8ef78c46c152125fac365ec216f140
Michael S. Noble <mnoble@broadinstitute.org>
2016-06-30 01:24:14 -0400

    first cut at adding aggregate cohorts

--- create_loadfile.py
+++ create_loadfile.py
@@ -8,13 +8,13 @@ Copyright (c) 2016 The Broad Institute, Inc.  All rights are reserved.
 gdc_mirror: this file is part of gdctools.  See the <root>/COPYRIGHT
 file for the SOFTWARE COPYRIGHT and WARRANTY NOTICE.
 
-@author: Timothy DeFreitas
+@author: Timothy DeFreitas, Michael S. Noble
 @date:  2016_05_25
 '''
 
 # }}}
-from __future__ import print_function
 
+from __future__ import print_function
 from GDCtool import GDCtool
 import logging
 import os
@@ -41,113 +41,218 @@ class create_loadfile(GDCtool):
                          help='Dice using metadata from a particular date.'\
                          'If omitted, the latest version will be used')
 
+        self.datestamp = None   # FIXME: rationalize this with datestamp optional arg
+        self.program = None
+
     def parse_args(self):
         opts = self.options
 
-        # Parse custom [load] section from config.
+        # Parse custom [firehose] section from configuration file.
         # This logic is intentionally omitted from GDCtool:parse_config, since
         # loadfiles are only useful to FireHose, and this tool is not distributed
-        if opts.config is not None:
+
+        if opts.config:
             cfg = ConfigParser.ConfigParser()
             cfg.read(opts.config)
 
+            # FIXME: this implicitly uses default lowercase behavior of config
+            #        parser ... which I believe we consider a bit more
+            #        For case sensitivity we can use the str() function as in:
+            #           cfg.optionxform = str
             if cfg.has_option('firehose', 'load_dir'):
                 self.load_dir = cfg.get('firehose', 'load_dir')
             if cfg.has_option('firehose', 'heatmaps_dir'):
                 self.heatmaps_dir = cfg.get('firehose', 'heatmaps_dir')
 
+            
+            self.aggregates = dict()
+            if cfg.has_section('aggregates'):
+                for aggr in cfg.options('aggregates'):
+                    aggr = aggr.upper()
+                    self.aggregates[aggr] = cfg.get('aggregates', aggr)
 
-        if opts.dice_dir is not None: self.dice_root_dir = opts.dice_dir
-        if opts.load_dir is not None: self.load_dir = opts.load_dir
+        if opts.dice_dir: self.dice_root_dir = opts.dice_dir
+        if opts.load_dir: self.load_dir = opts.load_dir
 
-    def create_loadfiles(self):
-        #Iterate over programs/projects in diced root
+    def inspect_data(self):
+
+        # GDC organizes data into Programs-->Projects-->Cases-->Data_Categories
+        # Given a processed (aka diced) mirror of the data corpus for a single
+        # program, this walker iterates over that dicing to build a map of:
+        #
+        #   all projects that have been mirrored/diced
+        #       then all cases within each project
+        #           then all samples for each case
+        #
+        # Note that "project" is effectively a proxy term for "disease cohort,"
+        # or more simply "cohort".
+        #
+        # The map created here is later used to generate so-called loadfiles.
+        # which are TSV tables where the first column of each row describes a
+        # unique tissue sample collected for a case (e.g. primary solid tumor,
+        # blood normal, etc) and each subsequent column points to a file of data
+        # derived from that tissue sample (e.g. copy number, gene expression,
+        # miR expression, etc).
+        #
+        # FIXME: I'm intentionally trying to describe this in a Firehose-agnostic
+        #        way, because I'm now leaning heavily in the direction that loadfile
+        #        generation may indeed be something of value to others outside the
+        #        Broad; because they are equivalent to sample freeze lists as used
+        #        in TCGA, for example.
+
         diced_root = os.path.abspath(self.dice_root_dir)
-        load_root = os.path.abspath(self.load_dir)
+        projects = dict()                       # dict of dicts, one per project
 
         for program in common.immediate_subdirs(diced_root):
-            prog_root = os.path.join(diced_root, program)
-            projects = common.immediate_subdirs(prog_root)
 
-            for project in projects:
-                #This dictionary contains all the data for the loadfile.
-                #Keys are the entity_ids, values are dictionaries for the columns in a loadfile
-                master_load_dict = dict()
+            # Auto-generated loadfiles should not mix data across >1 program
+            if self.program:
+                if program != self.program:
+                        raise ValueError("Loadfiles cannot span >1 program")
+            else:
+                self.program = program
 
-                proj_path = os.path.join(prog_root, project)
-                timestamp = latest_timestamp(proj_path, self.options.datestamp)
-                logging.info("Generating loadfile data for {0} -- {1}".format( project, timestamp))
-                # Keep track of the created annotations
-                annots = set()
+            program_dir = os.path.join(diced_root, program)
+            annotations = set()
 
-                metapath = get_diced_metadata(proj_path, timestamp)
+            for projname in common.immediate_subdirs(program_dir):
+
+                # Each project dict contains all the loadfile rows for the
+                # given project/cohort.  Keys are the entity_ids, values are
+                # dictionaries for the columns in a loadfile
+                project = dict()
+
+
+                projpath = os.path.join(program_dir, projname)
+                projdate = latest_timestamp(projpath, self.options.datestamp)
+
+                # Auto-generated loadfiles should not mix data across >1 datestamp
+                if self.datestamp:
+                    if projdate != self.datestamp:
+                        raise ValueError("Datestamp of {0} for {1} conflicts "\
+                                    "with existing datestamp of {2}\n"\
+                                    .format(projname, projdate, self.datestamp))
+                else:
+                    self.datestamp = projdate
+
+                logging.info("Inspecting data for {0} version {1}"\
+                                .format(projname, self.datestamp))
+
+                metapath = get_diced_metadata(projpath, self.datestamp)
                 with open(metapath) as metafile:
                     reader = csv.DictReader(metafile, delimiter='\t')
-                    #Stores the files and annotations for each case
+                    # Stores the files and annotations for each case
                     case_files = dict()
                     case_samples = dict()
+
                     for row in reader:
                         case_id = row['case_id']
                         annot = row['annotation']
                         filepath = row['file_name']
-                        annots.add(annot)
+                        annotations.add(annot)
 
                         if row['sample_type'] == "None":
-                            # This is a case-level file, save until all the
-                            # samples are known
+                            # This file exists only at the case-level (e.g.
+                            # clinical data) and so does not have a tissue
+                            # sample type (e.g. Primary Tumor).  Therefore
+                            # it will be attached to every sample of every
+                            # tissue type for this case; but that can only
+                            # be done after we know what those are, so we
+                            # save these files for now and back-fill later
                             case_files[case_id] = case_files.get(case_id, [])
                             case_files[case_id].append((filepath, annot))
                             continue
 
-                        #Add entry for this entity into the master load dict
-                        samp_id = sample_id(project, row)
+                        samp_id = sample_id(projname, row)
                         case_samples[case_id] = case_samples.get(case_id, [])
                         case_samples[case_id].append(samp_id)
 
-                        if samp_id not in master_load_dict:
-                            master_load_dict[samp_id] = master_load_entry(project, row)
-                        #Filenames in metadata begin with diced root,
+                        if samp_id not in project:
+                            project[samp_id] = master_load_entry(projname, row)
 
-                        master_load_dict[samp_id][annot] = filepath
+                        # Filenames in metadata begin with diced root
+                        project[samp_id][annot] = filepath
 
-                # Now all the samples are known, insert case-level files to each
+                # Now that all samples are known, back-fill case-level files for each
                 for case_id in case_samples:
                     # Insert each file into each sample in master_load_dict
                     files = case_files.get(case_id, [])
                     samples = case_samples.get(case_id, [])
                     for s in samples:
                         for f, annot in files:
-                            master_load_dict[s][annot] = f
+                            project[s][annot] = f
 
-                load_date_root = os.path.join(load_root, program, timestamp)
-                if not os.path.isdir(load_date_root):
-                    os.makedirs(load_date_root)
+                # Finally, retain this project data for later loadfile generation
+                projects[projname] = project
 
-                samples_loadfile_name = ".".join([project, timestamp, "Sample", "loadfile", "txt"])
-                sset_loadfile_name = ".".join([project, timestamp, "Sample_Set", "loadfile", "txt"])
-                samples_loadfile = os.path.join(load_date_root, samples_loadfile_name)
-                sset_loadfile = os.path.join(load_date_root, sset_loadfile_name)
+        return projects, sorted(annotations)
 
-                logging.info("Writing samples loadfile to " + samples_loadfile)
-                write_master_load_dict(master_load_dict, annots, samples_loadfile)
-                logging.info("Writing sample set loadfile to " + sset_loadfile)
-                write_sample_set_loadfile(samples_loadfile, sset_loadfile)
+    def generate_loadfiles(self, projname, annotations, cohorts):
+        # Generate a sample and sample_set loadfile for the given list of
+        # cohorts (i.e. GDC projects).  Note that singleton cohorts will
+        # have 1 entry in cohort list, and aggregate cohorts more than 1.
+        # Specifying each case (singleton vs aggregate) as a sequence 
+        # allows them both to be treated the same manner below.
 
-                #logging.info("Writing sample heatmaps")
-                #write_heatmaps(master_load_dict, annots, project, timestamp, load_date_root)
+        program = self.program
+        datestamp = self.datestamp
 
+        logging.info("Generating loadfile for {0}".format(projname))
+        loadfile_root = os.path.abspath(self.load_dir)
+        loadfile_root = os.path.join(loadfile_root, program, datestamp)
+        if not os.path.isdir(loadfile_root):
+            os.makedirs(loadfile_root)
+
+        # First: the samples loadfile
+        samples_loadfile = projname + "." + datestamp + ".Sample.loadfile.txt"
+        samples_loadfile = os.path.join(loadfile_root, samples_loadfile)
+        logging.info("Writing samples loadfile to " + samples_loadfile)
+        samples_lfp = open(samples_loadfile, 'w+')
+
+        # ... column headers
+        headers =  ["sample_id", "individual_id" ]
+        headers += ["sample_type", "tcga_sample_id"] + annotations
+        samples_lfp.write("\t".join(headers) + "\n")
+
+        # ... then the rows (samples) for each cohort (project) in cohorts list
+        for samples_in_this_cohort in cohorts:
+            write_samples(samples_lfp, headers, samples_in_this_cohort)
+
+        # Second: now the sample set loadfile, derived from the samples loadfile
+        sset_loadfile = projname + "." + datestamp + ".Sample_Set.loadfile.txt"
+        sset_loadfile = os.path.join(loadfile_root, sset_loadfile)
+        logging.info("Writing sample set loadfile to " + sset_loadfile)
+        write_sampleset(samples_lfp, sset_loadfile, projname)
+
+        #logging.info("Writing sample heatmaps")
+        #write_heatmaps(master_load_dict, annots, project, datestamp, load_date_root)
 
     def execute(self):
+
         super(create_loadfile, self).execute()
         self.parse_args()
         opts = self.options
-        print(dir(self))
+
         common.init_logging()
-        self.create_loadfiles()
+
+        # Discern what data is available for given program on given datestamp
+        (projects, annotations) = self.inspect_data()
+
+        # ... then generate singleton loadfiles (one per project/cohort)
+        for project in projects.keys():
+            self.generate_loadfiles(project, annotations, [projects[project]])
+
+        # ... lastly, generate any aggregate loadfiles (>1 project/cohort)
+        for aggr_name, aggr_definition in self.aggregates.items():
+            print("Aggregate: {0} = {1}".format(aggr_name, aggr_definition))
+            aggregate = []
+            for project in aggr_definition.split(","):
+                aggregate.append(projects[project])
+            self.generate_loadfiles(aggr_name, annotations, aggregate)
 
 # Could use get_metadata, but since the loadfile generator is separate, it makes sense to divorce them
-def get_diced_metadata(project_root, timestamp):
-    stamp_dir = os.path.join(project_root, "metadata", timestamp)
+def get_diced_metadata(project_root, datestamp):
+    stamp_dir = os.path.join(project_root, "metadata", datestamp)
 
     metadata_files = [f for f in os.listdir(stamp_dir)
                       if os.path.isfile(os.path.join(stamp_dir, f))
@@ -212,25 +317,38 @@ def master_load_entry(project, row_dict):
 
     return d
 
-def write_master_load_dict(ld, annots, outfile):
-    _FIRST_HEADERS = ["sample_id", "individual_id", "sample_type", "tcga_sample_id"]
-    annots = sorted(annots)
-    with open(outfile, 'w') as out:
-        #Header line
-        out.write("\t".join(_FIRST_HEADERS) + "\t" + "\t".join(annots)+"\n")
+def write_samples(fp, headers, samples):
+    # FIXME: touch up comments here
+    # Loop over sample ids, writing entries in outfile
+    # FIXME: presently assumes/requires at least one annot is defined
+    for sample_id in sorted(samples):
+        sample = samples[sample_id]
+        row = "\t".join([sample.get(h, "__DELETE__") for h in headers]) + "\n"
+        fp.write(row)
 
+def write_sampleset(samples_lfp, sset_filename, sset_name):
 
-        #Loop over sample ids, writing entries in outfile
-        #NOTE: requires at least one annot
-        for sid in sorted(ld):
-            this_dict = ld[sid]
-            line = "\t".join([this_dict[h] for h in _FIRST_HEADERS]) + "\t"
-            line += "\t".join([this_dict.get(a, "__DELETE__") for a in annots]) + "\n"
-            out.write(line)
+    # Rewind samples loadfile to beginning
+    samples_lfp.seek(0)
 
-# def write_heatmaps(ld, annots, project, timestamp, outdir):
+    # Create new sample set file
+    outfile = open(sset_filename, "w")
+    outfile.write("sample_set_id\tsample_id\n")
+
+    # Iteratively write each sample to multiple sample sets:
+    #   First, to the full cohort sample set (e.g. TCGA-COAD)
+    #   Then to the respective tissue-specific sample set (e.g TCGA-COAD-TP)
+    reader = csv.DictReader(samples_lfp, delimiter='\t')
+    for sample in reader:
+        samp_id = sample['sample_id']
+        # FIXME: clarify this code a little
+        sset_data = sset_name + "\t" + samp_id + "\n"
+        sset_data += sset_name + "-" + samp_id.split("-")[-1] + "\t" + samp_id + "\n"
+        outfile.write(sset_data)
+
+# def write_heatmaps(ld, annots, project, datestamp, outdir):
 #     rownames, matrix = _build_heatmap_matrix(ld, annots)
-#     draw_heatmaps(rownames, matrix, project, timestamp, outdir)
+#     draw_heatmaps(rownames, matrix, project, datestamp, outdir)
 #
 # def _build_heatmap_matrix(ld, annots):
 #     '''Build a 2d matrix and rownames from annotations and load dict'''
@@ -243,20 +361,6 @@ def write_master_load_dict(ld, annots, outfile):
 #
 #     return rownames, matrix
 
-
-def write_sample_set_loadfile(sample_loadfile, outfile):
-    sset_data = "sample_set_id\tsample_id\n"
-    with open(sample_loadfile) as slf:
-        reader = csv.DictReader(slf, delimiter='\t')
-        for row in reader:
-            samp_id = row['sample_id']
-            #This sample belongs to the cohort sample set
-            sset_data += samp_id.split("-")[0] + "\t" + samp_id + "\n"
-            #And the type-specific set
-            sset_data += samp_id.split("-")[0] + "-" + samp_id.split("-")[-1] + "\t" + samp_id + "\n"
-
-    with open(outfile, "w") as out:
-        out.write(sset_data)
 
 if __name__ == "__main__":
     create_loadfile().execute()
