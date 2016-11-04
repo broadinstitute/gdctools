e8e279b1d541b0d6e68287d1c18b0ce95d098ecc
Timothy DeFreitas <timdef@broadinstitute.org>
2016-11-04 11:35:08 -0400

    Oops -- add copy.py

---
+++ lib/convert/copy.py
@@ -0,0 +1,13 @@
+import shutil
+
+from lib.meta import tcga_id, diced_file_paths
+from lib.common import safeMakeDirs
+# Copy from mirror to dice dir
+
+def process(file_dict, mirror_path, dice_path):
+    _tcga_id = tcga_id(file_dict)
+    filepath = diced_file_paths(dice_path, file_dict)[0]
+    safeMakeDirs(dice_path)
+
+    # copy to new name in
+    shutil.copy(mirror_path, filepath)
