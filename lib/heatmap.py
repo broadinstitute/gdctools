bb444a574a8680b5ab6cd7906be9202bb587a766
Michael S. Noble <mnoble@broadinstitute.org>
2017-01-31 15:09:56 -0500

    lib/constants.py has been superseded by lib/common.py, but this file was not updated to reflect such

--- lib/heatmap.py
+++ lib/heatmap.py
@@ -22,7 +22,7 @@ from matplotlib.colors import ListedColormap, NoNorm
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib import font_manager
 
-from lib.constants import REPORT_DATA_TYPES
+from lib.common import REPORT_DATA_TYPES
 
 def draw_heatmaps(case_data, project, timestamp, diced_meta_dir):
     rownames, matrix = _build_heatmap_matrix(case_data)
