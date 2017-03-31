9c48e5ac46703e8fd60c19bb28f10c7e5f5b5fc0
Michael S. Noble <mnoble@broadinstitute.org>
2017-03-31 18:26:26 -0400

    added simple way to run python3 tests from CLI; also, echo python path at outset of test execution, to verify to caller that python/python3/etc is indeed being used

--- util/findPython.sh
+++ util/findPython.sh
@@ -5,6 +5,13 @@
 
 InstallDir=
 
+if [ -n "$1" ] ; then
+    PythonExe=$1
+    shift
+else
+    PythonExe=python
+fi
+
 # For convenience, give precedence to well-known directories @ Broad Institute
 BroadDirs="/local/firebrowse/latest /xchip/tcga/Tools/gdac/latest"
 for dir in $BroadDirs ; do
@@ -15,10 +22,10 @@ for dir in $BroadDirs ; do
 done
 
 if [ -z "$InstallDir" ] ; then
-    if [ ! -z "$VIRTUAL_ENV" ] ; then
+    if [ -n "$VIRTUAL_ENV" ] ; then
       InstallDir=$VIRTUAL_ENV
     else
-      Python=`type -P python`
+      Python=`type -P $PythonExe`
       if [ -n "$Python" ]; then
         InstallDir=`dirname $Python`
         InstallDir=`dirname $InstallDir`
@@ -27,7 +34,7 @@ if [ -z "$InstallDir" ] ; then
 fi
 
 if [ -z "$InstallDir" ] ; then
-    echo "Error: could not find a python installation to use" >&2
+    echo "Error: could not find a $PythonExe installation to use" >&2
     exit 1
 fi
 
