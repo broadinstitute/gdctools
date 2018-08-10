4d8b9520b280503a07a4471fc738b2e5df101590
David Heiman <dheiman@broadinstitute.org>
2018-08-10 17:32:00 -0400

    reordered washu_meth2magetab columns to more closely match the GDC

--- gdctools/gdc_dice.py
+++ gdctools/gdc_dice.py
@@ -617,10 +617,10 @@ def converter(converter_name):
 
     def washu_meth2magetab(file_dict, mirror_path, dice_path):
         gdac_tsv2magetab.process(file_dict, mirror_path, dice_path,
-                                 col_order=[0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,
-                                            18,19,20,21,22,23,24,25,26,27,28,29,30,31,
-                                            32,33,34,35,36,1], data_cols=[1],
-                                 id_func=meta.portion_id)
+                                 col_order=[0,2,3,4,25,26,27,13,5,6,7,8,9,10,
+                                            11,12,14,15,16,17,18,19,20,21,22,
+                                            23,24,28,29,30,31,32,33,34,35,36,1],
+                                 data_cols=[1], id_func=meta.portion_id)
 
     def unzip_fpkm2magetab(file_dict, mirror_path, dice_path):
         return _unzip(file_dict, mirror_path, dice_path, fpkm2magetab)
